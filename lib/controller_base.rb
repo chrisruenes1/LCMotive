591295b65c4e0f42b1b7be64882471447d3c926f
chrisruenes1 <ruenes.chris@gmail.com>
2016-08-16 11:45:31 -0400

    Build router server
    Uses object#instance_eval to evaluate router.draw block in context of router class. Uses capture groups to extract
    route params

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -7,9 +7,10 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, params)
     @req = req
     @res = res
+    @params = params.merge(req.params)
   end
 
   # Helper method to alias @already_built_response
@@ -42,7 +43,7 @@ class ControllerBase
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
-    template_file = "views/#{self.class.to_s.underscore}/#{template_name}.html.erb"
+    template_file = "views/#{self.class.to_s.underscore}/#{template_name.to_s}.html.erb"
     contents = File.read(template_file)
     erb = ERB.new(contents)
     html_content = erb.result(binding)
@@ -56,6 +57,7 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
-
+    self.send(name)
+    render(name) unless @already_built_response
   end
 end
--- lib/router.rb
+++ lib/router.rb
@@ -5,17 +5,21 @@ class Route
     @pattern = pattern
     @http_method = http_method
     @controller_class = controller_class
-    @action_name = @action_name
+    @action_name = action_name
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
-    return true if req.path =~ @pattern && req.request_method == @http_method.upcase
+    return true if req.path =~ @pattern && req.request_method == @http_method.to_s.upcase
   end
 
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
+    match_data = @pattern.match(req.path)
+    route_params = {}
+    match_data.names.each { |name| route_params[name] = match_data[name] }
+    @controller_class.new(req, res, route_params).invoke_action(action_name)
   end
 end
 
@@ -34,21 +38,26 @@ class Router
   # evaluate the proc in the context of the instance
   # for syntactic sugar :)
   def draw(&proc)
+    self.instance_eval(&proc)
   end
 
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
     define_method(http_method) do |pattern, controller_class, action_name|
-      add_route(patternk, http_method, controller_class, action_name)
+      add_route(pattern, http_method, controller_class, action_name)
     end
   end
 
   # should return the route that matches this request
   def match(req)
+    @routes.find {|route| route.matches?(req)}
   end
 
   # either throw 404 or call run on a matched route
   def run(req, res)
+    matched = match(req)
+    res.status = 404
+    res.errors = ("Could not find any action matching method #{req.request_method} for path #{req.path}")
   end
 end
