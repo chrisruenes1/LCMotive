cc2cbaddcd52c8f87d23df48ebf4cae91d30b8c7
chrisruenes1 <ruenes.chris@gmail.com>
2016-08-17 08:24:51 -0400

    Clean up comments and rename project

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -7,44 +7,36 @@ require_relative './flash'
 class ControllerBase
   attr_reader :req, :res, :params
 
-  # Setup the controller
   def initialize(req, res, params={})
     @req = req
     @res = res
     @params = params.merge(req.params)
   end
 
-  # Helper method to alias @already_built_response
   def already_built_response?
     @already_built_response
   end
 
-  # Set the response status code and header
   def redirect_to(url)
     raise 'ArgumentError', @already_built_response if @already_built_response
     @res["Location"] = url
     @res.status = 302
-    @already_built_response = "You have double-rendered and there is no recovering from that one!"
+    @already_built_response = "Double-render error"
     session.store_session(@res)
     flash.store_flash(@res)
     @res
   end
 
-  # Populate the response with content.
-  # Set the response's content type to the given type.
-  # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
     raise 'ArgumentError', @already_built_response if @already_built_response
     @res["Content-Type"] = content_type
     @res.write(content)
-    @already_built_response = "You have double-rendered and there is no recovering from that one!"
+    @already_built_response = "Double-render error"
     session.store_session(@res)
     flash.store_flash(@res)
     @res
   end
 
-  # use ERB and binding to evaluate templates
-  # pass the rendered html to render_content
   def render(template_name)
     template_file = "views/#{self.class.to_s.underscore}/#{template_name.to_s}.html.erb"
     contents = File.read(template_file)
@@ -53,7 +45,6 @@ class ControllerBase
     render_content(html_content, 'text/html')
   end
 
-  # method exposing a `Session` object
   def session
     @session ||= Session.new(@req)
   end
@@ -62,7 +53,6 @@ class ControllerBase
     @flash ||= Flash.new(@req)
   end
 
-  # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
     self.send(name)
     render(name) unless @already_built_response
--- lib/router.rb
+++ lib/router.rb
@@ -9,13 +9,10 @@ class Route
     @action_name = action_name
   end
 
-  # checks if pattern matches path and method matches request method
   def matches?(req)
     return true if req.path =~ @pattern && req.request_method == @http_method.to_s.upcase
   end
 
-  # use pattern to pull out route params (save for later?)
-  # instantiate controller and call controller action
   def run(req, res)
     match_data = @pattern.match(req.path)
     route_params = {}
@@ -31,36 +28,29 @@ class Router
     @routes = []
   end
 
-  # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
     @routes.push(Route.new(pattern, method, controller_class, action_name))
   end
 
-  # evaluate the proc in the context of the instance
-  # for syntactic sugar :)
   def draw(&proc)
     self.instance_eval(&proc)
   end
 
-  # make each of these methods that
-  # when called add route
   [:get, :post, :put, :delete].each do |http_method|
     define_method(http_method) do |pattern, controller_class, action_name|
       add_route(pattern, http_method, controller_class, action_name)
     end
   end
 
-  # should return the route that matches this request
   def match(req)
     @routes.find {|route| route.matches?(req)}
   end
 
-  # either throw 404 or call run on a matched route
   def run(req, res)
     matched = match(req)
     if !matched
       res.status = 404
-      res["Error"] = "Could not find any action matching method #{req.request_method} for path #{req.path}"
+      res.write("Could not find any action matching method #{req.request_method} for path #{req.path}")
     else
       matched.run(req, res)
     end
